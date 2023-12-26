f0f022a58eef069cee51206d4364604e95ce120a
yuki <fu08yu20ko07@icloud.com>
2023-12-26 10:27:13 +0900

    first commit

---
+++ ThreeMinAlert/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace ThreeMinAlert;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Alarm();
+	}
+	private async void Alarm(){
+		var time = int.Parse(entry.Text);
+
+		// for(var i = time; time > 0; i--){
+		// 	var Remtime = Convert.ToString(i);
+		// 	RemainingTime.Text = Remtime;
+		// 	await Task.Delay(1000);
+		// }
+		while(time != 0){
+			var Remtime = Convert.ToString(time);
+			RemainingTime.Text = Remtime;
+			await Task.Delay(1000);
+			time--;
+		}
+		AlarmUp();
+	}
+	private void AlarmUp(){
+		var title = "time up";
+		label.Text = title;
+	}
+}
+
